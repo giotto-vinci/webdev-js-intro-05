ba222d9e3ae0a7c9f0628949e6136fa9187e2c03
Roderick Fitzgerald <liber6schol@outlook.com>
2025-01-19 02:39:41 -0500

    finished experimenting in index.js and updated task list in readme.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+let shoppingList = ["Coffee", "Sugar", "Hazelnut Cream", "Apples", "Mandarins", "Kiwi"];
+
+console.log(shoppingList[1]);
+shoppingList.shift();
+console.log(shoppingList);
+
+shoppingList.pop();
+console.log(shoppingList);
+
+shoppingList.push("Bananas");
+console.log(shoppingList);
+
+shoppingList.shift();
+console.log(shoppingList);
+
+shoppingList[0] = "Milk";
+console.log(shoppingList);
+
+let shoppingCart = ["1", 3, 'Water', true, "Lemons"];
+console.log(shoppingCart);
+
+console.log(typeof[0]);
+console.log(typeof[2]);
+*/
