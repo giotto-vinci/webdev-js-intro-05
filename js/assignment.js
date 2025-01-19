ad1c414d60e0c5c7c63483fe1da7912bc1bff819
Roderick Fitzgerald <liber6schol@outlook.com>
2025-01-19 03:46:03 -0500

    I wrote the code for each step but the assignment.js file had bugs upon opening it as far as I saw, could easily be wrong, updated tasks but the code doesn't work yet.

--- js/assignment.js
+++ js/assignment.js
@@ -10,6 +10,8 @@ const todos = [
 
 // Declare your two immutable variables
 
+let shiftValue;
+let popValue;
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
@@ -22,43 +24,56 @@ const answerSixEl = document.getElementById("answer-six");
 const submissionBtn = document.getElementById("submission-btn");
 
 function updateAnswerOne() {
-    
+    console.log(todos);
 }
 
 function updateAnswerTwo() {
-
+    console.log(todos[2]);
 }
 
 function removeLastValue() {
-
+    todos.pop();
+    let popValue = array.pop;
+    console.log(todos);
+    todos.reverse();
+    console.log(todos);
 }
 
 function removeFirstValue() {
-
+    todos.shift();
+    let shiftValue = array.shift;
 }
 
 function addShiftAndPopValues() {
-
+    todos.push("shiftValue", "popValue");
 }
 
 function updateAnswerFour() {
-
+    console.log(todos);
 }
 
 function reverseTodoList() {
-
+    todos.reverse();
 }
 
 function updateAnswerFive() {
-
+    console.log(todos);
 }
 
 function updateAnswerSix() {
-
+    console.log(todos);
 }
 
 function render() {
-
+    updateAnswerOne();
+    updateAnswerTwo();
+    removeLastValue();
+    removeFirstValue();
+    addShiftAndPopValues();
+    updateAnswerFour();
+    reverseTodoList();
+    updateAnswerFive();
+    updateAnswerSix();
 }
 
 submissionBtn.addEventListener("click", function () {
